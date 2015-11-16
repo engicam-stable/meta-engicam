b3b0a96c82d7afefb9df3efcb9a3d1df10401993
matteo.lisi <matteo.lisi@engicam.com>
2015-11-16 09:38:12 +0100

    remove old files

--- tools/build_script/buildboot_icorex.sh
+++
@@ -1,7 +0,0 @@
-. /opt/poky/1.6.2/environment-setup-cortexa9hf-vfp-neon-poky-linux-gnueabi
-export ARCH=arm
-export CROSS_COMPILE=$TARGET_PREFIX
-unset LDFLAGS
-export LOADADDR=0x80008000
-make icore_mx6sx_config
-make -j 8
